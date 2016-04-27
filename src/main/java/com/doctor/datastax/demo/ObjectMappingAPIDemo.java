ee1b993b77091061506d1d7b0b1f4d5c9b421369
sdcuike <sdcuike@aol.com>
2016-04-27 17:41:49 +0800

    Object-mapping API demo

--- src/main/java/com/doctor/datastax/demo/ObjectMappingAPIDemo.java
+++ src/main/java/com/doctor/datastax/demo/ObjectMappingAPIDemo.java
@@ -1,7 +1,7 @@
 package com.doctor.datastax.demo;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
@@ -11,29 +11,46 @@ import org.slf4j.LoggerFactory;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
-import com.datastax.driver.mapping.Mapper;
 import com.datastax.driver.mapping.MappingManager;
+import com.datastax.driver.mapping.Result;
+import com.doctor.datastax.demo.OpenStatistic.OpenStatisticAccessor;
 
 /**
+ * Object-mapping API demo
+ * 
  * @author sdcuike
  *         //TODO:
  *         Create At 2016年4月27日 下午4:33:37
  * 
  * @see http://docs.datastax.com/en/latest-java-driver/java-driver/reference/crudOperations.html
+ *      http://docs.datastax.com/en/latest-java-driver/common/drivers/reference/accessorAnnotatedInterfaces.html
  * 
  */
 public class ObjectMappingAPIDemo {
 
     public static void main(String[] args) {
         ObjectMappingAPIDemo demo = new ObjectMappingAPIDemo();
-
         demo.connect("127.0.0.1");
         demo.createSchema();
-        Mapper<OpenStatistic> mapper = demo.getMapper();
+        OpenStatisticAccessor openStatisticAccessor = demo.getOpenStatisticAccessor();
+        System.out.println("=========");
+        Result<OpenStatistic> all = openStatisticAccessor.getAll();
+        all.forEach(System.out::println);
+        System.out.println("=========");
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime openTime = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), now.getHour(), 0, 0, 0);
+        Date openTimeT = Date.from(openTime.toInstant(ZoneOffset.ofHours(8)));
+        openStatisticAccessor.add(openTimeT, "test");
 
+        System.out.println("=========");
+        all = openStatisticAccessor.getAll();
+        all.forEach(System.out::println);
+
+        System.out.println("=========");
+        all = openStatisticAccessor.getAll(openTimeT);
+        all.forEach(System.out::println);
+        demo.close();
     }
 
     protected Logger log = LoggerFactory.getLogger(getClass());
@@ -41,7 +58,7 @@ public class ObjectMappingAPIDemo {
     private Session session;
     private Cluster cluster;
     final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private Mapper<OpenStatistic> mapper;
+    private OpenStatisticAccessor openStatisticAccessor;
 
     public void connect(String node) {
         cluster = Cluster.builder().addContactPoint(address).build();
@@ -52,7 +69,7 @@ public class ObjectMappingAPIDemo {
             log.info("Datacenter:{},Host:{},Rack:{}", host.getDatacenter(), host.getAddress(), host.getRack());
         }
         session = cluster.connect();
-        mapper = new MappingManager(session).mapper(OpenStatistic.class);
+
     }
 
     public void createSchema() {
@@ -60,49 +77,20 @@ public class ObjectMappingAPIDemo {
                 + "= {'class':'SimpleStrategy', 'replication_factor':1};";
 
         session.execute(createKeyspace);
-        String createTable = "create columnfamily if not exists open_statistic (" +
+        String createTable = "create columnfamily if not exists test_doctor.open_statistic (" +
                 "email_recipient text ,open_time timestamp,count counter,primary key(email_recipient,open_time)); ";
         session.execute(createTable);
+        MappingManager mappingManager = new MappingManager(session);
+        openStatisticAccessor = mappingManager.createAccessor(OpenStatisticAccessor.class);
 
-    }
-
-    public void loadData() {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime dateTime = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), now.getHour(), 0, 0, 0);
-        String date = dateTime.format(dateTimeFormatter);
-        session.execute("update test_doctor.m_cf set count =count+1 where time = '" + date + "' and id = " + "6");
-    }
-
-    public void querySchema() {
-        ResultSet results = session.execute("SELECT * FROM test_doctor.m_cf ");
-        System.out.println(String.format("%-30s\t%-20s\t%-20s\n%s", "title", "album", "artist",
-                "-------------------------------+-----------------------+--------------------"));
-
-        for (Row row : results) {
-            Date date = row.getTimestamp("time");
-            LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.of("Asia/Shanghai"));
-            System.out.println(row.getLong("id") + "  ===" + localDateTime.format(dateTimeFormatter) + "===" + row.getLong("count"));
-        }
-
-        System.out.println();
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime dateTime = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), now.getHour(), 0, 0, 0);
-        String date = dateTime.format(dateTimeFormatter);
-
-        ResultSet set = session.execute("SELECT * FROM test_doctor.m_cf  where time = '" + date + "'  ALLOW FILTERING;");
-        for (Row row : set) {
-            Date date2 = row.getTimestamp("time");
-            LocalDateTime localDateTime = LocalDateTime.ofInstant(date2.toInstant(), ZoneId.of("Asia/Shanghai"));
-            System.out.println(row.getLong("id") + "  ===" + localDateTime.format(dateTimeFormatter) + "===" + row.getLong("count"));
-        }
     }
 
     public Session getSession() {
         return session;
     }
 
-    public Mapper<OpenStatistic> getMapper() {
-        return mapper;
+    public OpenStatisticAccessor getOpenStatisticAccessor() {
+        return openStatisticAccessor;
     }
 
     public void close() {
--- src/main/java/com/doctor/datastax/demo/OpenStatistic.java
+++ src/main/java/com/doctor/datastax/demo/OpenStatistic.java
@@ -1,13 +1,21 @@
 package com.doctor.datastax.demo;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.mapping.Result;
+import com.datastax.driver.mapping.annotations.Accessor;
 import com.datastax.driver.mapping.annotations.Column;
+import com.datastax.driver.mapping.annotations.Param;
+import com.datastax.driver.mapping.annotations.Query;
 import com.datastax.driver.mapping.annotations.Table;
+import com.datastax.driver.mapping.annotations.Transient;
 
 /**
  * @author sdcuike
@@ -16,12 +24,13 @@ import com.datastax.driver.mapping.annotations.Table;
  */
 @Table(keyspace = "test_doctor", name = "open_statistic")
 public class OpenStatistic {
-
+    @Transient
+    public static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     @Column(name = "email_recipient")
     private String emailRecipient;
 
     @Column(name = "open_time")
-    private LocalDateTime openTime;
+    private Date openTime;
 
     private Long count;
 
@@ -33,11 +42,11 @@ public class OpenStatistic {
         this.emailRecipient = emailRecipient;
     }
 
-    public LocalDateTime getOpenTime() {
+    public Date getOpenTime() {
         return openTime;
     }
 
-    public void setOpenTime(LocalDateTime openTime) {
+    public void setOpenTime(Date openTime) {
         this.openTime = openTime;
     }
 
@@ -62,6 +71,20 @@ public class OpenStatistic {
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this);
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(openTime.toInstant(), ZoneId.of("Asia/Shanghai"));
+        return "emailRecipient:" + emailRecipient + ";openTime:" + localDateTime.format(dateTimeFormatter) + ";count:" + count;
+    }
+
+    @Accessor
+    public interface OpenStatisticAccessor {
+
+        @Query("update test_doctor.open_statistic set count =count+1 where open_time = :openTime and email_recipient = :emailRecipient")
+        ResultSet add(@Param("openTime") Date openTime, @Param("emailRecipient") String emailRecipient);
+
+        @Query("SELECT * FROM test_doctor.open_statistic")
+        public Result<OpenStatistic> getAll();
+
+        @Query("SELECT * FROM test_doctor.open_statistic where open_time =:openTime ALLOW FILTERING;")
+        public Result<OpenStatistic> getAll(@Param("openTime") Date openTime);
     }
 }
